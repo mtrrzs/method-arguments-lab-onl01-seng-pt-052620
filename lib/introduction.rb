4d10ba4b7a45bd2f85f90eac03faa6aba62f1cc4
Mario Terrazas <mtrrzs@hotmail.com>
2020-05-31 19:18:51 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+introduction("Josh")
+
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learnig to program in #{language}."
+end
+introduction("Dan", "Ember.js")
+
